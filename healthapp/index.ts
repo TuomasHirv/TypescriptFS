cfcb32804fddd54134bf9be6539905169dc2cfad
TuomasHirv <hirvonentume@gmail.com>
2026-08-15 15:44:11 +0300

    Second attempt

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -29,7 +29,7 @@ app.get("/bmi", (req, res) => {
   return res.status(200).json(responseValues);
 });
 
-const PORT = process.env.PORT ?? 3003;
+const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
