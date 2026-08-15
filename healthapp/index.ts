b2bcd368d0bbd8da3b179b860f7e1446e01a8de7
TuomasHirv <hirvonentume@gmail.com>
2026-08-15 15:40:38 +0300

    Attempting to fix CI port mismatch

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -29,7 +29,7 @@ app.get("/bmi", (req, res) => {
   return res.status(200).json(responseValues);
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT ?? 3003;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
