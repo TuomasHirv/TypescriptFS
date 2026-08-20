2f787641f5d26e68ac9a9589f9700b941cd30d68
TuomasHirv <hirvonentume@gmail.com>
2026-08-20 19:18:21 +0300

    Added zod schema

--- patientor/backend/src/routes/patientsRouter.ts
+++ patientor/backend/src/routes/patientsRouter.ts
@@ -1,9 +1,10 @@
 import express, { type Response } from "express";
-import type { patientNoSSN } from "../types.ts";
+import { z } from "zod";
+import { newPatientSchema, type patientNoSSN } from "../types.ts";
+
 const router = express.Router();
 
 import patientService from "../services/patientService.ts";
-import parseNewPatient from "../utils.ts";
 router.get("/", (_req, res: Response<patientNoSSN[]>) => {
   const data = patientService.getData();
   res.send(data);
@@ -11,10 +12,13 @@ router.get("/", (_req, res: Response<patientNoSSN[]>) => {
 
 router.post("/", (req, res) => {
   try {
-    const patient = parseNewPatient(req.body);
+    const patient = newPatientSchema.parse(req.body);
     const newPatient = patientService.addPatient(patient);
     res.status(200).json(newPatient);
   } catch (error) {
+    if (error instanceof z.ZodError) {
+      res.status(400).send({ error: error.issues });
+    }
     res.status(500).json(error);
   }
 });
--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export const Gender = {
   Male: "male",
   Female: "female",
@@ -5,6 +7,15 @@ export const Gender = {
 } as const;
 
 export type Gender = (typeof Gender)[keyof typeof Gender];
+
+export const newPatientSchema = z.object({
+  name: z.string(),
+  dateOfBirth: z.iso.date(),
+  ssn: z.string(),
+  gender: z.enum(Gender),
+  occupation: z.string(),
+});
+
 export interface diagnosis {
   code: string;
   name: string;
@@ -19,7 +30,7 @@ export interface patient {
   occupation: string;
 }
 
-export type newPatient = Omit<patient, "id">;
+export type newPatient = z.infer<typeof newPatientSchema>;
 
 // I found that without: ssn?: never; TypeScript doesn't warn me if I return patient type instead
 // Something about patient being a subtype of patientNoSSN so I added it here I think its smart.
